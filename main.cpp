315c3e5e5d34d85e01e9524c7c3a2b8bf5117c6f
adthelad <aroberts@hep.ph.liv.ac.uk>
2016-07-29 13:50:26 +0100

    Reads in generic map

--- main.cpp
+++ main.cpp
@@ -1,22 +1,53 @@
-#include <iostream>
-#include <fstream>
-
-using namespace std;
+#include<iostream>
+#include<fstream>
+#include<vector>
 
 int main() {
-    cout << "in Main" << endl;
 
-    ifstream routeMap;
-    routeMap.open ("/Users/willturner/Desktop/route.txt");
+  std::cout << "Welcome to TrafficRouter!" << std::endl;
+  
+  // Read the route and throw an error if something goes wrong
+  std::ifstream routeMap;
+  routeMap.open ("route.txt");
+  if(!routeMap){std::cout << "Error: Route map not found" << std::endl;}
 
-    string myArray[14];
+  // Find out some information about the map file; width and height.
+  unsigned int numberoflines = 0;
+  unsigned int maxwidth = 0;
+  
+  std::string line;
+  while(getline(routeMap, line)){
+    numberoflines++;
+    if(line.length()>maxwidth){maxwidth=line.length();}
+  }
 
-    for (int i = 0; i < 14; i++){
-        getline(routeMap, myArray[i], '*');
+  std::cout << "The world map is " << numberoflines << " elements tall" << 
+    " and is at most " << maxwidth << " elements wide" << std::endl;
 
-        cout << myArray[i] ;
+  // Create a vector for storing the map - MapVector[i][j]. [0][0] is the top left corner.
+  std::vector<std::vector<char> > MapVector;
+  MapVector.resize(maxwidth);
+  for(int i=0; i<MapVector.size(); ++i){
+    MapVector[i].resize(numberoflines);
+  }
+  
+  // Read the route into the vector
+  routeMap.clear();
+  routeMap.seekg(0, std::ios::beg);
+  
+  {unsigned int linenumber = 0;
+    while(getline(routeMap,line)){
+      for(int i=0; i<line.length(); ++i){
+	MapVector[i][linenumber] = line[i];
+      }
+      linenumber++;
     }
+  }
+  routeMap.close();
 
-    routeMap.close();
-    return 0;
+  // We now have the world loaded into the vector MapVector[][].
+  
+  
+
+  return 0;
 }
